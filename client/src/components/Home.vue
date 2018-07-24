80bf5ce291e838dd2daa68d826f77153e7553294
unknown <mwegagideon@gmail.com>
2018-07-24 13:54:41 +0300

    routing in client and server folder updted

---
+++ client/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h1>{{display}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            display:" This message shows that the Home component is working! ;)"
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
+
