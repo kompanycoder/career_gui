aa5240e4060776dc0f86a42eab816665dde687e9
unknown <mwegagideon@gmail.com>
2018-07-24 13:03:44 +0300

    updated the server files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const bodyParser = require(body-parser.json());
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/home', (req, res) => {
+    res.send(
+      [{
+        title: "Hello World!",
+        description: "Hi there! How are you?"
+      }]
+    );
+});
+
+app.listen(process.env.PORT || 8081);
+
+console.log('Server is running');
